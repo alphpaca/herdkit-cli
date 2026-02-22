fa00e6a8cff57c0776d5057769decab8cd3d85a9
Jacob Tobiasz <jakub.tobiasz@icloud.com>
2026-02-22 20:11:32 +0100

    :tada: Initial commit.

---
+++ src/cli.tsx
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+import { Command } from "commander";
+import { render, Text, Box } from "ink";
+import React from "react";
+
+const program = new Command();
+
+program
+    .name("herdkit")
+    .description("PHP monorepo management tool")
+    .version("0.1.0");
+
+program
+    .command("hello", { isDefault: true })
+    .description("Say hello")
+    .action(() => {
+        render(
+            <Box flexDirection="column" padding={1}>
+                <Text bold color="green">
+                    🦙 Herdkit
+                </Text>
+                <Text dimColor>PHP monorepo management tool</Text>
+            </Box>
+        );
+    });
+
+program.parse();
